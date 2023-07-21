0802e5ced86b5bffabd7c8061e4146b09184edef
Pierre0412 <pierre@esteron.net>
2023-07-21 12:25:16 +0200

    initial commit

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ dictionnaire = dictionnaire.get("results")
 for i in dictionnaire:
     # print(i)
     if i.get("acteAttachment").get("filename").endswith("pdf"):
-        print(f'{i.get("decision")} / {i.get("nature")} /  {i.get("objet")} / {i.get("acteAttachment").get("filename")} '
+        print(f'{i.get("decision")} / {i.get("nature")} /  {i.get("objet")} / {i.get("acteAttachment").get("filename")}'
               f'https://stela3k.sictiam.fr/api/acte/public/{i.get("uuid")}/file/stamped')
+
+# ajout de commentaire test
